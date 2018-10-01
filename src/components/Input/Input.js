b2121c64110820760a69fd37bb83fea5385bcc34
StanislavkoP <stas8327634@gmail.com>
2018-10-01 10:49:28 +0300

    added input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Input.css'
+
+const Input = (props) => (
+	<input
+	 	type="text" 
+		className={classes.NameRecipe}
+	/>
+)
+export default Input;
